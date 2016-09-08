720bc9fdd9951022c6f399b2d9a7b32e6a4d9bab
puronglong <1240429156@qq.com>
2016-09-09 00:47:20 +0800

    no message

--- green.py
+++ green.py
@@ -37,4 +37,4 @@ def daily_commit(start_date, end_date):
 
 
 if __name__ == '__main__':
-    daily_commit(datetime.date(2016, 9, 7), datetime.date(2016, 9, 7))
+    daily_commit(datetime.date(2016, 9, 7), datetime.date(2016, 9, 8))
